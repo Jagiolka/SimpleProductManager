82642ee7cbff07b64eadddf90fef153b5a42fb1c
Ingo Jagiolka <Ingo.Jagiolka@gmx.de>
2023-11-24 10:31:16 +0100

    init Solution

---
+++ SimpleProductManager/SimpleProductManager.Gui/App.xaml.cs
@@ -0,0 +1,47 @@
+﻿namespace SimpleProductManager.Gui;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Windows;
+
+public partial class App : Application
+{
+  private readonly IHost _host;
+
+  public App()
+  {
+    _host = Host.CreateDefaultBuilder()
+      .ConfigureAppConfiguration((context, builder) =>
+      {
+        var env = context.HostingEnvironment.EnvironmentName;
+        builder.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                     .AddJsonFile($"appsettings.{context.HostingEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true);
+      })
+      .ConfigureServices((context, services) =>
+      {
+        services.AddLogging();
+        services.AddSingleton<MainWindow>();
+      })
+
+      .Build();
+  }
+
+  protected override async void OnStartup(StartupEventArgs e)
+  {
+    await _host.StartAsync();
+    var mainWindow = _host.Services.GetRequiredService<MainWindow>();
+    mainWindow.Show();
+    base.OnStartup(e);
+  }
+
+  protected override async void OnExit(ExitEventArgs e)
+  {
+    using (_host)
+    {
+      await _host.StopAsync(TimeSpan.FromSeconds(5));
+    }
+    base.OnExit(e);
+  }
+}
---
+++ SimpleProductManager/SimpleProductManager.Gui/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace SimpleProductManager.Gui.ViewModel;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public partial class MainWindowViewModel : ObservableObject
+{
+
+}
